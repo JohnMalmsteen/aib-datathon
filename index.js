085785d31bd485a355306dba682d050a48a3ecdf
JohnMalmsteen <g00305339@gmit.ie>
2015-11-07 13:11:31 +0000

    Switched over to mongo and threw in the first CSV file

--- index.js
+++ index.js
@@ -2,24 +2,32 @@
 var express = require('express');
 var fs = require('fs');
 var parse = require('csv-parse');
-var async = require('async');
+ test = require('assert');
 // Creete a couch connector instance
 
 // Create a HTTP server app.
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-var nodeCouchDB = require("node-couchdb");
-var couch = new nodeCouchDB("localhost", 5984);
-// insert a document
-function insertCustomer(cust){
-  couch.insert("customers", cust, function (err, resData) {
-      if (err)
-          return console.error(err);
-      //console.dir(resData)
-  });
-}
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
+var url = 'mongodb://localhost:27017/datathon';
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+// insert a document
+var insertDocument = function(db, cust, callback) {
+   db.collection('customers').insertOne( cust, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback(result);
+  });
+};
 
 // body parser is needed to parse the data from the body
 var bodyParser = require('body-parser');
@@ -27,17 +35,43 @@ app.use(bodyParser());
 
 
 var inputFile='Balances.csv';
-
+var customers = [];
 var parser = parse({delimiter: ','}, function (err, data) {
-  async.eachSeries(data, function (line, callback) {
+  data.forEach(function (line) {
     // do something with the line
     var customer = {};
     customer._id = line[0];
     customer.balance = parseInt(line[1]);
-    insertCustomer(customer);
-    callback();
+    customers.push(customer);
+  });
+
+  MongoClient.connect(url, function(err, db) {
+  // Get the collection
+    var col = db.collection('customers');
+    col.insertMany(customers, function(err, r) {
+      test.equal(null, err);
+      console.log(r.insertedCount);
+      // Finish up test
+      db.close();
+    });
   });
 });
+
+
+var incomeparser = parse({delimiter: ','}, function (err, data) {
+  data.forEach(function (line) {
+    // do something with the line
+    var customer = {};
+    customer._id = line[0];
+    customer.income = parseInt(line[1]);
+    customer.payday = parseInt(line[2]);
+    for(i = 0; i < 100000; i++){
+
+    }
+    updateCustomer(customer._id, customer.income, customer.payday);
+  });
+});
+
 fs.createReadStream(inputFile).pipe(parser);
 
 // middleware:
