46d6b395ccb16b8653747b5f5d93d59b678990ff
Ronan Connolly <hi@ronanconnolly.ie>
2015-11-07 10:20:58 +0000

    added package.json & index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+// Import express to create and configure the HTTP server.
+var express = require('express');
+
+// Create a HTTP server app.
+var app = express();
+
+
+
+// middleware:
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'null'); // null or url
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get('/datathon', function(req, res) {
+  var result = [];
+  result.push({id: 0, header: "Datathon", info: "Customer Insights"});
+
+  res.contentType('application/json');
+  res.send(JSON.stringify(result));
+});
